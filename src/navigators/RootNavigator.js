785bf56823e6af54a9c744cb84f74b49fcff9ec8
rakesh-arulvel <rakesh.arulvel@freshworks.com>
2018-05-18 04:31:00 +0530

    nav bar icons

--- src/navigators/RootNavigator.js
+++ src/navigators/RootNavigator.js
@@ -4,7 +4,8 @@ import {
   TabBarBottom,
   createStackNavigator,
 } from 'react-navigation';
-
+import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import ParkNow from '../screens/ParkNow';
 import Panel from '../screens/Panel';
@@ -18,6 +19,7 @@ import SignUp from '../screens/SignUp.js';
 import SignIn from '../screens/SignIn.js';
 import Welcome from '../screens/Welcome.js';
 
+
 const PanelTab = StackNavigator({
   Panel: { screen: Panel },
   Bookings: { screen: Bookings },
@@ -29,19 +31,37 @@ const PanelTab = StackNavigator({
   mode: 'modal',
 });
 
-export const TabNav = TabNavigator({
-  ParkNow: { screen: ParkNow },
-  Panel: PanelTab,
-  ParkLater: { screen: ParkLater },
-}, {
+export const TabNav = TabNavigator(
+  {
+    Now: { screen: ParkNow },
+    Home: PanelTab,
+    Later: { screen: ParkLater },
+  }, 
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Now') {
+          iconName = `md-flash`;
+        } else if (routeName === 'Home') {
+          iconName = `md-home`;
+        }
+        else if (routeName === 'Later') {
+          iconName = `md-clock`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+
   tabBarOptions: {
-  	labelStyle: {
-  	  fontWeight: '600',
-  	  marginBottom: 5,
-  	},
-    activeTintColor: 'red'
+    activeTintColor: '#4266b2',
+    inactiveTintColor: 'gray',
   },
-    initialRouteName: 'Panel',
+    initialRouteName: 'Home',
   tabBarComponent: TabBarBottom,
   tabBarPosition: 'bottom',
 });
